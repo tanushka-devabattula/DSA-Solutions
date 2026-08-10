3bcca6001fd13cf55da9d537443da2471bf97b01
Tanushka Devabattula <tanushka7525@gmail.com>
2026-08-10 10:59:07 +0530

    Time: 2 ms (70.9%), Space: 55.6 MB (60.25%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int can=nums[0],count=1;
+        for(int i=1;i<nums.length;i++)
+        {
+          if(can==nums[i])
+          count++;
+          else
+          count--;
+          
+            if(count==0 && can!=nums[i])
+            {
+                can=nums[i];
+                count=1;
+            }
+          
+        }
+
+        return can;
+    }
+}
